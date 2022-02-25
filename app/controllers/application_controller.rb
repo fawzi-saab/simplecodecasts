1ac4bfd956292c3567bd06f36bc07d73b6d3fcd9
fawzisaab <reddevil_ut@hotmail.com>
2022-02-25 15:17:39 +0000

    fixed stripe version error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
    before_filter :configure_permitted_parameters, if: :devise_controller?
   
   protected
-    def configure_permitted_paramaters
+    def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :stripe_card_token, :email, :password, :password_confirmation])
   end
 end
