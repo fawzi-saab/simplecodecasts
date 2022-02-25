7ec88d788690c32c8c527e18d6fd38e1f4bb4356
fawzisaab <reddevil_ut@hotmail.com>
2022-02-25 09:26:29 +0000

    added modefied devise for stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  before_filter :configure_permitted_paramaters, if: :devise_controller?
+  
+  protected
+  
+    def configure_permitted_paramaters
+        devise_paramater_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :Password, :password_confirmation) }
+    end
 end
