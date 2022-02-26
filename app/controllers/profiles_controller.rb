39256aa46e948865b395526a8e188ee8f2802855
fawzisaab <reddevil_ut@hotmail.com>
2022-02-26 13:45:33 +0000

    added profiles new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,7 @@
 class ProfilesController < ApplicationController
-    
+    def new
+        #form where a user can fill out his own profile
+        @user = User.find(params[:user_id])
+        @profile = @user.build_profile    #build_profile can be used cause we established a connection in the routes file (nested resources)
+    end
 end
