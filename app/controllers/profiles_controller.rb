37071025eeb8b04000853d1845463586a100ba02
fawzisaab <reddevil_ut@hotmail.com>
2022-02-28 09:25:15 +0000

    added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,25 @@
 class ProfilesController < ApplicationController
+   
     def new
         #form where a user can fill out his own profile
         @user = User.find(params[:user_id])
-        @profile = @user.build_profile    #build_profile can be used cause we established a connection in the routes file (nested resources)
+        @profile = @user.build_profile    #build_profile can be used cause we established a connection in the routes file (nested resources) we used it here to build a blank profile
     end
+    
+   def create
+       @user = User.find(params[:user_id])
+       @profile = @user.build_profile(profile_params)           #we use build_profile here to store the actual data that the user typed
+       if @profile.save
+           flash[:success] = "profile updated!"
+           redirect_to user_path(params[:user_id])
+       else
+           render action: :new
+       end
+   end
+    
+    
+    private
+        def profile_params
+            params.require(:profile).permit(:first_name, :last_name, :job_title, :contact_email, :phone_number, :description)
+        end
 end
